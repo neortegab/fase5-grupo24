84543056cd4798d9a163b3f8e8a4842d0af381c1
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-24 19:58:41 -0500

    react app for dsw course

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Fase 5 - React - Grupo 24</h1>
+      </div>
+      <div className="card">
+        <p>Holman Estih Fonseca Rojas</p>
+        <p>Nelson Enrique Ortega Beltran</p>
+        <p>Edgardo Leonel Leal Ariza</p>
+        <p>Clara Esther Martinez Abaunza</p>
+      </div>
+      <a className="read-the-docs" href='index_grupal.html'>
+        Clic aquí para acceder a la página.
+      </a>
+    </>
+  )
+}
+
+export default App
